d21d4c2e873f180fbf761b5b930dfdb0d2cac37a
Valen <valenpschool@gmail.com>
2023-12-30 12:46:43 -0300

    lifecycle methods

--- 2-react-basics/src/App.js
+++ 2-react-basics/src/App.js
@@ -2,33 +2,54 @@ import './App.css';
 import { Component } from 'react'
 
 class App extends Component {
-state = {
-  posts: [
-    {
-      id: 1,
-      title: "título 1",
-      "description": "Olá"
-    },    {
-      id: 2,
-      title: "título 2",
-      "description": "Bom dia"
-    },    {
-      id: 3,
-      title: "título 3",
-      "description": "Prazer"
-    },
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "título 1",
+        "description": "Olá"
+      }, {
+        id: 2,
+        title: "título 2",
+        "description": "Bom dia"
+      }, {
+        id: 3,
+        title: "título 3",
+        "description": "Prazer"
+      }
+    ]
+  };
+  timeoutUpdate = null;
+  componentDidMount() {
+    this.handleTimeout();
+  }
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+  handleTimeout = () => {
+    const {posts, counter} = this.state;
+    posts[0].title = "Mudança";
+    this.timeoutUpdate = setTimeout(() => {
+      // assim que o componente montar, busca dados na API
+      // configuração do estado a partir disso
+    this.setState({posts, counter: counter + 1});
+    }, 1000);
+  }
 
-  ]
-    };
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
     return <div className="App">
-    {posts.map((post) => (
-      <div key={post.id}>
-        <h2>{post.title}</h2>
-        <p>{post.description}</p>
-      </div>
-    ))}
+      <p>{counter}</p>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
+        </div>
+      ))}
     </div>
   }
 }
