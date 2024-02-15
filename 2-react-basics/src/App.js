1e33f5a94e723db6bca48f2c1d1dc5e0993c526f
Valen <valenpschool@gmail.com>
2024-02-15 07:52:11 -0300

    aulas 1 e 2 de props (recommitadas)

--- 2-react-basics/src/App.js
+++ 2-react-basics/src/App.js
@@ -1,47 +1,28 @@
 import './App.css';
 import { Component } from 'react'
+import { loadPosts } from './utils/load-posts'
+import { Posts } from './components/Posts'
 
 class App extends Component {
   state = {
-   
+
     posts: [
     ]
   };
 
- componentDidMount() {
-  this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
   loadPosts = async () => {
-  const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
-  const [posts, photos] = await Promise.all([postsResponse, photosResponse])
-
-  const postsJson = await posts.json();
-  const photosJson = await photos.json();
-  const postsAndPhotos = postsJson.map((post, index) => {
-    return {...post, cover: photosJson[index].url}
-  })
-  this.setState({posts: postsAndPhotos})
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos })
   }
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
     return <section className='container'>
-
-    <div className="posts">
-      <p>{counter}</p>
-      {posts.map((post) => (
-      <div className='post'>
-        <img src={post.cover} alt='post cover'/>
-        <div key={post.id} className='post-content'>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          </div>
-          </div>
-
-      ))}
-    </div>
+      <Posts posts={posts} />
     </section>
   }
 }
---
+++ 2-react-basics/src/components/PostCard/index.jsx
@@ -0,0 +1,9 @@
+export const PostCard = ({ title, cover, body, id }) => (
+    <div className="post">
+      <img src={cover} alt={title} />
+      <div className="post-content">
+        <h2>{title}</h2>
+        <p>{body}</p>
+      </div>
+    </div>
+  );
