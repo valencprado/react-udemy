a3dfb5f8a1cda95aa2a1ac4bb6e32f0eb26ce3ed
Valentina Prado <valenpschool@gmail.com>
2024-09-11 17:22:47 -0300

    useRef, useMemo

--- 3-react-hooks/src/App.js
+++ 3-react-hooks/src/App.js
@@ -1,23 +1,56 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import {useEffect, useMemo, useRef, useState} from 'react'
+import P from 'prop-types'
+
+const Post = ({post, handleClick}) => {
+  return (<div key={post.id}>
+    <h1 style={{fontSize: '1rem'}} onClick={() => handleClick(post.title)}>{post.title}</h1>
+    <p>{post.body}</p>
+  </div>)
+}
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string
+  }),
+  handleClick: P.func,
+}
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  useEffect(() => {
-    console.log('ComponentDidUpdate');
-  });
-  useEffect(() => {
-    console.log('ComponentDidMount');
-  }, []);
-  useEffect(() => {
-    console.log('State changed: ', counter);
-  }, [counter]);
+  const [posts, setPosts] = useState([])
+  const [value, setValue] = useState('')
+  const input = useRef(null)
+console.log("Paai")
 
+const handleClick = (value) => {
+  setValue(value)
+}
+
+useEffect(() => {
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(r => r.json())
+.then(r => setPosts(r))
+}, [])
+
+useEffect(() => {
+  input.current.focus()
+
+}, [value])
   return (
     <div className="App">
-      <h1>Contador {counter}</h1>
-      {/* eslint-disable-next-line no-const-assign */}
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+<p>
+  <input ref={input} type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+</p>
+
+
+    {useMemo(() => {
+      return posts.length > 0 &&
+      posts.map(post => {
+        return <Post key={post.id} handleClick={handleClick} post={post}/>
+       })
+    }, [posts])
+   }
     </div>
   );
 }
